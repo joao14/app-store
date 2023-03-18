66b7682b182cc872210064e169a97ebf2ce89bd3
Alexander Merino <barcealex1014@hotmail.com>
2023-03-17 21:52:19 -0500

    feat: web client update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,13 @@ import { ClientService } from './service/clientservice';
 export class AppComponent implements OnInit {
   title = 'app-store';
 
-  clients: Client[]=[]
+  clients: Client[] = []
 
-  client: Client={}
+  client: Client = {}
 
-  clientDialog: boolean=false
+  clientDialog: boolean = false
 
-  constructor(private clientService: ClientService,  private messageService: MessageService) {
+  constructor(private clientService: ClientService, private messageService: MessageService) {
   }
 
   ngOnInit() {
@@ -26,27 +26,36 @@ export class AppComponent implements OnInit {
   }
 
   getallClients = async () => {
-    this.clients=[]
+    this.clients = []
     await this.clientService.getAllClients().then(data => {
-      console.log(data)
-      this.clients=data
+      this.clients = data
     }).catch(err => {
       console.log("Error")
       console.log(err)
     })
   }
 
-  new= async()=>{
-    this.clientDialog=true
+  new = async () => {
+    this.clientDialog = true
   }
 
-  confirmAddClient=async()=>{
+  confirmAddClient = async () => {
     await this.clientService.addClient(this.client).then(async data => {
-      console.log(data)
-      this.clients=data
+      this.clients = data
       await this.getallClients()
-      this.clientDialog=false
+      this.clientDialog = false
       this.messageService.add({ key: 'tst', severity: 'success', summary: 'Store Message', detail: 'Client has been added' });
+    }).catch(err => {
+      console.log("Error")
+      console.log(err)
+    })
+  }
+
+  deleteClient = async (client: any) => {
+    await this.clientService.deleteClient(client.id, client).then(async data => {
+      await this.getallClients()
+      this.clientDialog = false
+      this.messageService.add({ key: 'tst', severity: 'success', summary: 'Store Message', detail: 'Client has been deleted' });
     }).catch(err => {
       console.log("Error")
       console.log(err)
--- src/app/service/clientservice.ts
+++ src/app/service/clientservice.ts
@@ -4,21 +4,27 @@ import { throwError } from 'rxjs';
 import { Client } from '../domain/client';
 
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: "root" })
 export class ClientService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    getAllClients = () => {
-        return this.http.get<any>('https://localhost:7111/client/list')
-            .toPromise()
-            .then(data => data);
-    }
+  getAllClients = () => {
+    return this.http.get<any>('https://localhost:7111/client')
+      .toPromise()
+      .then(data => data);
+  }
 
-    addClient = (client: Client) => {
-      return this.http.post<any>('https://localhost:7111/client/add', client)
-          .toPromise()
-          .then(data => data);
+  addClient = (client: Client) => {
+    return this.http.post<any>('https://localhost:7111/client', client)
+      .toPromise()
+      .then(data => data);
+  }
+
+  deleteClient = (idClient: string, client: any) => {
+    return this.http.delete<any>('https://localhost:7111/client/' + idClient, client)
+      .toPromise()
+      .then(data => data);
   }
 
 }
